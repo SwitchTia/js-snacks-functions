14646dc45d67f9f4f3f3124fe83936794434f4d4
SwitchTia <oblasser.tatiana@gmail.com>
2025-11-04 14:20:48 +0100

    Feat: 03 Conta vocali completato

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -1,13 +1,29 @@
-/* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
+/* Scrivi una funzione che accetti 
+una stringa 
+e restituisca il numero di vocali contenute al suo interno */
 
 const word = 'javascript';
 
 
 // Dichiara la funzione qui.
 
+const countVowels = (string) => {
+
+    const vowels = ["a","o","u","e","i","y"];
+    let count = 0;
+
+    for (let i = 0; i < string.length - 1; i++) {
+        for(let j = 0; j < vowels.length - 1; j++){
+            if(string[i] === vowels[j])
+                count++;
+        }
+    }
+    return count;
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(countVowels(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
