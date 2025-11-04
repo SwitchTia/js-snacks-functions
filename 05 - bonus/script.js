475793d87b209be8365a231d069583f108c306a5
SwitchTia <oblasser.tatiana@gmail.com>
2025-11-04 18:17:46 +0100

    Feat: Bonus wip

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -1,4 +1,5 @@
-/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito. 
+/* Scrivi una funzione che accetti una stringa contenente un nome 
+e restituisca un saluto seguito dal nome fornito. 
 Il saluto deve essere :
 buongiorno se è mattina (fino alle 13), 
 buon pomeriggio se è pomeriggio (fino alle 17) 
@@ -9,10 +10,23 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
+const getCurrentHour = () =>{
+    const now = new Date();
+    const hours = now.getHours();
+    return hours;
+}
 
+const saluteProperTime = (stringName) => {
+    let currentHour = getCurrentHour();
+    //console.log(currentHour);
+    
+
+
+    return ;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(saluteProperTime(name));
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
